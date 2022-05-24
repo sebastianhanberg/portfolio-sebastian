7f0d701997afc938db1fe288136cfabd888ff568
SEBASTIANHANBERG <SEBASTIAN.HANBERG@HOTMAIL.COM>
2022-05-24 03:33:44 +0200

    about

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,10 +1,55 @@
 import React from 'react'
 import './about.css'
+import MeAbout from '../../assets/me-about.jpg'
+import { FaAward } from 'react-icons/fa'
+import { FiUsers } from 'react-icons/fi'
+import { AiOutlineFolderOpen } from 'react-icons/ai'
 
 const About = () => {
     return (
         <section id="about">
-            About
+            <h5>Get To Know</h5>
+            <h2>About Me</h2>
+
+            <div className='container about__container'>
+                <div className='about__me'>
+                    <div className='about__me-image'>
+                        <img src={MeAbout} alt="About Me Image" />
+                    </div>
+                </div>
+
+                <div className="about__content">
+                    <div className="about__cards">
+                        <article className='about__card'>
+                            <FaAward className='about__icon' />
+                            <h5>Experience</h5>
+                            <small>2+ years</small>
+                        </article>
+
+                        <article className='about__card'>
+                            <FiUsers className='about__icon' />
+                            <h5>Clients</h5>
+                            <small>2 & counting</small>
+                        </article>
+
+                        <article className='about__card'>
+                            <AiOutlineFolderOpen className='about__icon' />
+                            <h5>Projects</h5>
+                            <small>10+ completed</small>
+                        </article>
+                    </div>
+
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                        Tenetur asperiores expedita, cum minus, rem ratione autem
+                        perspiciatis reiciendis officia obcaecati debitis id quaerat,
+                        dolorum earum est voluptatibus deleniti dolores ex!
+                    </p>
+                    <a href="#contact" className='btn btn-primary'>Let's Talk!</a>
+
+                </div>
+            </div>
+
         </section>
     )
 }
--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -2,8 +2,8 @@ import React, { useState } from 'react'
 import './nav.css'
 import { AiOutlineHome } from 'react-icons/ai'
 import { AiOutlineUser } from 'react-icons/ai'
-import { HiOutlineLightBulb } from 'react-icons/hi'
-import { GiBookmark } from 'react-icons/gi'
+import { ImBooks } from 'react-icons/im'
+import { AiOutlineFolderOpen } from 'react-icons/ai'
 import { BiMessageDots } from 'react-icons/bi'
 
 const Nav = () => {
@@ -12,8 +12,8 @@ const Nav = () => {
         <nav>
             <a href="#" onClick={() => setActiveNav('#')} className={activeNav === '#' ? 'active' : ''} alt="Homepage"><AiOutlineHome /></a>
             <a href="#about" onClick={() => setActiveNav('#about')} className={activeNav === '#about' ? 'active' : ''} alt="About Me"><AiOutlineUser /></a>
-            <a href="#experience" onClick={() => setActiveNav('#experience')} className={activeNav === '#experience' ? 'active' : ''} alt="My Experience"><HiOutlineLightBulb /></a>
-            <a href="#portfolio" onClick={() => setActiveNav('#portfolio')} className={activeNav === '#portfolio' ? 'active' : ''} alt="My Portfolio"><GiBookmark /></a>
+            <a href="#experience" onClick={() => setActiveNav('#experience')} className={activeNav === '#experience' ? 'active' : ''} alt="My Experience"><ImBooks /></a>
+            <a href="#portfolio" onClick={() => setActiveNav('#portfolio')} className={activeNav === '#portfolio' ? 'active' : ''} alt="My Portfolio"><AiOutlineFolderOpen /></a>
             <a href="#contact" onClick={() => setActiveNav('#contact')} className={activeNav === '#contact' ? 'active' : ''} alt="Contact Me"><BiMessageDots /></a>
         </nav>
     )
